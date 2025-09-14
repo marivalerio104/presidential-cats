2ff03239282d7b73234c2aff383ef981b1ae1645
marielav1043 <139282125+marivalerio104@users.noreply.github.com>
2025-09-14 07:27:22 -0600

    Create cats.js

---
+++ src/cats.js
@@ -0,0 +1,247 @@
+export const cats = [
+  {
+    id: 1,
+    name: 'José María Castro Madriz',
+    term: '1848-1849',
+  },
+  {
+    id: 2,
+    name: 'Juan Rafael Mora Porras',
+    term: '1849-1859',
+  },
+  {
+    id: 3,
+    name: 'José María Montealegre Fernández',
+    term: '1859-1863',
+  },
+  {
+    id: 4,
+    name: 'Jesús Jiménez Zamora',
+    term: '1863-1866',
+  },
+  {
+    id: 5,
+    name: 'José María Castro Madriz',
+    term: '1866-1868',
+  },
+  {
+    id: 6,
+    name: 'Jesús Jiménez Zamora',
+    term: '1868-1870',
+  },
+  {
+    id: 7,
+    name: 'Bruno Carranza Ramírez',
+    term: '1870-1870',
+  },
+  {
+    id: 8,
+    name: 'Tomás Guardia Gutiérrez',
+    term: '1870-1876',
+  },
+  {
+    id: 9,
+    name: 'Aniceto Esquivel Sáenz',
+    term: '1876-1876',
+  },
+  {
+    id: 10,
+    name: 'Vicente Herrera Zeledón',
+    term: '1876-1877',
+  },
+  {
+    id: 11,
+    name: 'Tomás Guardia Gutiérrez',
+    term: '1877-1882',
+  },
+  {
+    id: 12,
+    name: 'Saturnino Lizano Gutiérrez',
+    term: '1882-1882',
+  },
+  {
+    id: 13,
+    name: 'Próspero Fernández Oreamuno',
+    term: '1882-1885',
+  },
+  {
+    id: 14,
+    name: 'Bernardo Soto Alfaro',
+    term: '1885-1889',
+  },
+  {
+    id: 15,
+    name: 'José Joaquín Rodríguez Zeledón',
+    term: '1890-1894',
+  },
+  {
+    id: 16,
+    name: 'Rafael Yglesias Castro',
+    term: '1894-1902',
+  },
+  {
+    id: 17,
+    name: 'Ascensión Esquivel Ibarra',
+    term: '1902-1906',
+  },
+  {
+    id: 18,
+    name: 'Cleto González Víquez',
+    term: '1906-1910',
+  },
+  {
+    id: 19,
+    name: 'Ricardo Jiménez Oreamuno',
+    term: '1910-1914',
+  },
+  {
+    id: 20,
+    name: 'Alfredo González Flores',
+    term: '1914-1917',
+  },
+  {
+    id: 21,
+    name: 'Federico Tinoco Granados',
+    term: '1917-1919',
+  },
+  {
+    id: 22,
+    name: 'Juan Bautista Quirós Segura',
+    term: '1919-1919',
+  },
+  {
+    id: 23,
+    name: 'Francisco Aguilar Barquero',
+    term: '1919-1920',
+  },
+  {
+    id: 24,
+    name: 'Julio Acosta García',
+    term: '1920-1924',
+  },
+  {
+    id: 25,
+    name: 'Ricardo Jiménez Oreamuno',
+    term: '1924-1928',
+  },
+  {
+    id: 26,
+    name: 'Cleto González Víquez',
+    term: '1928-1932',
+  },
+  {
+    id: 27,
+    name: 'Ricardo Jiménez Oreamuno',
+    term: '1932-1936',
+  },
+  {
+    id: 28,
+    name: 'León Cortés Castro',
+    term: '1936-1940',
+  },
+  {
+    id: 29,
+    name: 'Rafael Ángel Calderón Guardia',
+    term: '1940-1944',
+  },
+  {
+    id: 30,
+    name: 'Teodoro Picado Michalski',
+    term: '1944-1948',
+  },
+  {
+    id: 31,
+    name: 'Otilio Ulate Blanco',
+    term: '1949-1953',
+  },
+  {
+    id: 32,
+    name: 'José María Figueres Ferrer',
+    term: '1953-1958',
+  },
+  {
+    id: 33,
+    name: 'Mario Echandi Jiménez',
+    term: '1958-1962',
+  },
+  {
+    id: 34,
+    name: 'Francisco Orlich Bolmarcich',
+    term: '1962-1966',
+  },
+  {
+    id: 35,
+    name: 'José Joaquín Trejos Fernández',
+    term: '1966-1970',
+  },
+  {
+    id: 36,
+    name: 'José María Figueres Ferrer',
+    term: '1970-1974',
+  },
+  {
+    id: 37,
+    name: 'Daniel Oduber Quirós',
+    term: '1974-1978',
+  },
+  {
+    id: 38,
+    name: 'Rodrigo Carazo Odio',
+    term: '1978-1982',
+  },
+  {
+    id: 39,
+    name: 'Luis Alberto Monge Álvarez',
+    term: '1982-1986',
+  },
+  {
+    id: 40,
+    name: 'Óscar Arias Sánchez',
+    term: '1986-1990',
+  },
+  {
+    id: 41,
+    name: 'Rafael Ángel Calderón Fournier',
+    term: '1990-1994',
+  },
+  {
+    id: 42,
+    name: 'José María Figueres Olsen',
+    term: '1994-1998',
+  },
+  {
+    id: 43,
+    name: 'Miguel Ángel Rodríguez Echeverría',
+    term: '1998-2002',
+  },
+  {
+    id: 44,
+    name: 'Abel Pacheco de la Espriella',
+    term: '2002-2006',
+  },
+  {
+    id: 45,
+    name: 'Óscar Arias Sánchez',
+    term: '2006-2010',
+  },
+  {
+    id: 46,
+    name: 'Laura Chinchilla Miranda',
+    term: '2010-2014',
+  },
+  {
+    id: 47,
+    name: 'Luis Guillermo Solís Rivera',
+    term: '2014-2018',
+  },
+  {
+    id: 48,
+    name: 'Carlos Alvarado Quesada',
+    term: '2018-2022',
+  },
+  {
+    id: 49,
+    name: 'Rodrigo Chaves Robles',
+    term: '2022-2026',
+  }
+];
